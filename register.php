cd63ba0bbee0f3dc607d69523272e670e7b01393
841973620 <841973620@qq.com>
2022-04-30 13:48:01 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,18 +1,18 @@
 <?php
-    if (!preg_match("/^[a-zA-Z ]*$/", $_POST[userName]))
+	    if (!preg_match("/^[a-zA-Z ]*$/", $_POST[userName]))
     {
-		echo "Illegal user name.";
+	echo "Illegal user name.";
         header("Refresh:2;url=register.html");
     }
     else if ($_POST[userName] == NULL || $_POST[pswd] == NULL)
     {
-		echo "Password cannot be empty.";
+	echo "Password cannot be empty.";
         header("Refresh:2;url=register.html");
     }
-	else if($_POST[pswd] != $_POST[confirm])
+    else if($_POST[pswd] != $_POST[confirm])
         echo "The passwords entered are inconsistent.";
     else
-	{
+    {
         include('config.php');
         $userNameSHA256 = hash("sha256", $_POST[userName]);
         mysql_select_db($dbName, $connect);
@@ -20,10 +20,10 @@
 
         if ($sql == 1)
         {
-			$path = "user/$_POST[userName]";
-			mkdir(iconv("UTF-8", "GBK", $path), true);
-			echo "New account created successfully.";
-			header("Refresh:2;url=index.php");
+	    $path = "user/$_POST[userName]";
+	    mkdir(iconv("UTF-8", "GBK", $path), true);
+	    echo "New account created successfully.";
+	    header("Refresh:2;url=index.php");
         }
         else
             die('Error: ' . mysql_error());
