e83bf8eca94e0ba030e3ca6cbac25e849c1a75a3
841973620 <841973620@qq.com>
2022-05-02 04:53:36 +0800

    Update new.php

--- new.php
+++ new.php
@@ -3,11 +3,11 @@
         header("Refresh:0;url=index.php");
     else
     {
-        $connect = mysql_connect($_POST[dbServerName], $_POST[dbUserName], $_POST[dbPassword], $_POST[dbName]);
+        $connect = mysql_connect($_POST[dbServerName], $_POST[dbUserName], $_POST[dbPassword], "storage");
         if (!$connect)
             die('Could not connect: ' . mysql_error());
-        mysql_select_db($dbName, $connect);
         mysql_query("CREATE SCHEMA `storage`");
+        mysql_select_db("storage", $connect);
         mysql_query("CREATE TABLE `storage`.`user` ( `userName` VARCHAR(16) NOT NULL, `userNameSHA256` VARCHAR(256) NOT NULL, `pswd` VARCHAR(32) NULL, `op` VARCHAR(1) NOT NULL, PRIMARY KEY (`userName`), UNIQUE INDEX `userNamw_UNIQUE` (`userName` ASC), UNIQUE INDEX `userNameSHA256_UNIQUE` (`userNameSHA256` ASC))");
         $sql = mysql_query("INSERT INTO `storage`.`user` (`userName`, `userNameSHA256`, `op`) VALUES ('Guest', '5ed8944a85a9763fd315852f448cb7de36c5e928e13b3be427f98f7dc455f141', '2')");
         if ($sql != 1)
@@ -16,7 +16,7 @@
         {
             mkdir(iconv("UTF-8", "GBK", "user/Guest"), true);
             $file = fopen("config.php", "w") or die("Unable to open file!");
-            $txt = "<?php\n    \$dbServerName = \"$_POST[dbServerName]\";\n    \$dbUserName = \"$_POST[dbUserName]\";\n    \$dbPassword = \"$_POST[dbPassword]\";\n    \$dbName = "storage";\n    \$notAllowedExts = array(\"php\", \"html\", \"js\", \"css\", \"htm\");\n    \$connect = mysql_connect(\$dbServerName, \$dbUserName, \$dbPassword, \$dbName);\n    if (!\$connect)\n        die('Could not connect: ' . mysql_error());\n?>\n";
+            $txt = "<?php\n    \$dbServerName = \"$_POST[dbServerName]\";\n    \$dbUserName = \"$_POST[dbUserName]\";\n    \$dbPassword = \"$_POST[dbPassword]\";\n    \$dbName = \"storage\";\n    \$notAllowedExts = array(\"php\", \"html\", \"js\", \"css\", \"htm\");\n    \$connect = mysql_connect(\$dbServerName, \$dbUserName, \$dbPassword, \$dbName);\n    if (!\$connect)\n        die('Could not connect: ' . mysql_error());\n?>\n";
             fwrite($file, $txt);
             fclose($file);
             unlink('new.html');
