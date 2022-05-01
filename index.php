8ead7ee4158966e0ad4aee108463dbdcd86ce82f
841973620 <841973620@qq.com>
2022-05-02 03:37:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-    if ($_COOKIE[encryptedUserName] == NULL)
+    if (file_exists('config.php') == False)
+        header("Refresh:0;url=new.html");
+    else if ($_COOKIE[encryptedUserName] == NULL)
         header("Refresh:0;url=login.html");
     else
         header("Refresh:0;url=user.php");
