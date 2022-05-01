6906cf64ba03cb8f3afb8f152082d1df248b9d67
Z841973620 <841973620@qq.com>
2022-05-02 02:57:28 +0800

    log message

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (file_exists('config.php') == False)
-        header("Refresh:0;url=new.html");
-    else if ($_COOKIE[encryptedUserName] == NULL)
-        header("Refresh:0;url=login.html");
-    else
-        header("Refresh:0;url=user.php");
-?>
--- user/backDir.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dirName = "$_COOKIE[dirName]";
-	while (substr($dirName, -1) != '/' && $dirName != NULL)
-		$dirName = substr_replace($dirName ,"", -1);
-	$dirName = substr_replace($dirName ,"", -1);
-	setcookie('dirName', $dirName, time()+300);
-	header("Refresh:0;url=file.php");
-?>
